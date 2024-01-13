061cd8da85ab611aa77c5da6ab4a485b6a0bd0ce
AllissonFelipe <154332420+AllissonFelipe@users.noreply.github.com>
2024-01-13 19:55:29 -0300

    Add files via upload

---
+++ basic1/Ex020.java
@@ -0,0 +1,19 @@
+package basic1;
+
+import java.util.*;
+
+public class Ex020 {
+    public static void main(String[] args) {
+        int decimal;
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Conversor de decimal para hexadecimal.");
+        System.out.println("\nDigite o valor decimal: ");
+        decimal = teclado.nextInt();
+        String valorHexa = Integer.toHexString(decimal);
+        System.out.println("O valor em hexadecimal é: " + valorHexa);
+        teclado.close();
+    }
+}
+
+
