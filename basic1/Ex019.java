8f36d7c4a24c4d9e615141caad4a80efaca55f28
AllissonFelipe <154332420+AllissonFelipe@users.noreply.github.com>
2024-01-13 19:26:37 -0300

    Add files via upload

---
+++ basic1/Ex019.java
@@ -0,0 +1,28 @@
+package basic1;
+
+import java.util.*;
+
+public class Ex019 {
+    public static void main(String[] args) {
+        int numeroDecimal, valorBinario;
+        StringBuffer bin = new StringBuffer();
+
+        Scanner teclado = new Scanner(System.in);
+
+        try {
+            System.out.println("Convertor de decimal para binário.");
+            System.out.println("\nDigite um valor decimal: ");
+            numeroDecimal = teclado.nextInt();
+
+            while (numeroDecimal != 0) {
+                valorBinario = numeroDecimal%2;
+                bin.append(valorBinario);
+                numeroDecimal/=2;  
+            }
+            System.out.println("O valor em binário é: "+bin.reverse().toString()+".");
+            teclado.close();
+        } catch (Exception e) {
+            System.out.println("Ops! Ocorreu um erro." +e );
+        }        
+    }
+}
