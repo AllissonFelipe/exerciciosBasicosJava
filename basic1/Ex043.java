7c461105c73a17fa20361728d9a6195a989d825d
AllissonFelipe <154332420+AllissonFelipe@users.noreply.github.com>
2024-01-20 16:19:37 -0300

    Add files via upload

---
+++ basic1/Ex043.java
@@ -0,0 +1,7 @@
+public class Ex043 {
+    public static void main(String[] args) {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        System.out.println("\nTwinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!\n\n");
+    }
+}
